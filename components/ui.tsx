11a17ac7834fe895c4a7f807b37b2038eb357cd5
epoeck <ezequielpoecksilva@hotmail.com>
2025-08-29 15:40:13 -0300

    Update ui.tsx

--- components/ui.tsx
+++ components/ui.tsx
@@ -59,7 +59,7 @@ export const BackIcon: React.FC<{className?: string}> = ({ className }) => (
 
 export const ShuffleIcon: React.FC<{className?: string}> = ({ className }) => (
     <svg xmlns="http://www.w3.org/2000/svg" className={className} fill="none" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M16.023 9.348h4.992v-.001M2.985 19.644v-4.992m0 0h4.992m-4.993 0l3.181 3.183a8.25 8.25 0 0011.667 0l3.181-3.183m-4.991-2.691v4.992h-4.992m0 0l-3.181-3.183a8.25 8.25 0 0111.667 0l3.181 3.183" />
+      <path strokeLinecap="round" strokeLinejoin="round" d="M7 16l-4-4m0 0l4-4m-4 4h18m-7 4l4 4m0 0l-4 4m4-4H3" />
     </svg>
 );
 
